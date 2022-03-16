6670e53d3c6158c8a02aed31e4df27fb5dc9d0a0
Mark <mark.n.bacon.jr@gmail.com>
2022-03-16 10:28:36 -0400

    commit tree 0c8e498e3a01ce3c0a03d6149da8931aca784ae5

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+
+// filter((arr) => {
+//     let string = string.toLowerCase();
+//     (arrNames === string)
+// }
+// return arrNames
+// )
+
+// const findMatching = arrNames.filter(arrName => (arrName.toLowerCase() === string.toLowerCase()))
+
+function findMatching(arrNames, string){
+   return arrNames.filter(arrName => arrName.toLowerCase() === string.toLowerCase())}
+    
+    
+function fuzzyMatch (arrNames, string){
+    return arrNames.filter(arrName => arrName.charAt(0) === string.charAt(0))
+}
+
+function matchName(arrNames, string){
+    return arrNames.filter(arrName => arrName.name === string)
+}
